9ef2f97766e38e54013346b974cd4142da4c9572
Regis Alphonse <regisalphonse@gmail.com>
2021-11-04 20:37:11 +0100

    Base Factry

--- js/Datas.js
+++ js/Datas.js
@@ -1,4 +1,9 @@
 //recupere les donnes presentes dans mon fichier json
 
-
-console.log("test");
+let url ="https://s3-eu-west-1.amazonaws.com/course.oc-static.com/projects/Front-End+V2/P5+Javascript+%26+Accessibility/FishEyeData.json";
+fetch(url).then(function(resp){
+    return resp.json();
+}).then(function(data)
+{
+    console.log(data)
+});
