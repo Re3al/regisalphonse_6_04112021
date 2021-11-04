658953587c0266b4a192480b0bcc11a12fc7750b
Regis Alphonse <regisalphonse@gmail.com>
2021-11-04 20:14:20 +0100

    Base Factry

--- js/Factory/FactoryPattern.js
+++ js/Factory/FactoryPattern.js
@@ -1,9 +1,23 @@
 //design patter Factory
 
+function photographer(name)
+{
+    this.name = name;
+    this.id = id;
+    this.image = image;
+    this.likes = likes;
+    this.date = date;
+    this.tags = tags;
+    this.price = price;
+}
+
 function PhotographerFactory()
 {
     this.create = (name,id) =>
     {
-
+        return new Photographer(name);
     }
 }
+
+const photographerFactory = new PhotographerFactory();
+const photographers = [];
