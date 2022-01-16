36f02d3b9954f10052d82f0a38410739d8b2f956
Regis Alphonse <regisalphonse@gmail.com>
2022-01-16 09:53:10 +0100

    Identation du code

--- scripts/factories/CardDOM.js
+++ scripts/factories/CardDOM.js
@@ -70,8 +70,7 @@ class CardDOM
         article.appendChild(h3);
         article.appendChild(taglineText);
         article.appendChild(priceText);
-        /*h2.insertAdjacentHTML('afterbegin',
-        '<a href="`?id=${photographer.id}`;">');*/        
+        
         return (article);
     }
     getMediatype()
@@ -86,46 +85,55 @@ class CardDOM
       }
     }
     getMediaCardDOM(data) {
-        //const { name, portrait,city,tagline,price,id } = data;
     
         const userId = this.getUser(data._photographerId);
         const picture = `assets/photographers/${userId}/${data._image}`;
-        if(data._video){
-          const video = `assets/photographers/${userId}/${data._video}`;
+   
+        const video = `assets/photographers/${userId}/${data._video}`;
+        const article = document.createElement( 'article' );
+
+        if(data._image)
+        {
+          
+          const img = document.createElement( 'img' );
+          img.setAttribute("src", picture);
+            article.appendChild(img);
+   
+          let imgAricle = data.image;
+        }
+        if(data._video)
+        {
+          
+          const videotag = document.createElement( 'video' );
+          const videosource = document.createElement( 'source' );
+          videosource.setAttribute("src", video);
+          article.appendChild(videotag);
+          videotag.appendChild(videosource);
+   
+          let imgAricle = data.image;
         }
         
-    
-            const article = document.createElement( 'article' );
-            const img = document.createElement( 'img' );
-            const videotag = document.createElement( 'video' );
-            const videosource = document.createElement( 'source' );
-            img.setAttribute("src", picture);
-            videosource.setAttribute("src", picture);
-            const h2 = document.createElement( 'h2' );
-            const a = document.createElement( 'a' );
-            a.setAttribute("href", "photographer.html?id=" + data._id); 
-            a.innerHTML = data._title;     
-            h2.appendChild(a);
-            const h3 = document.createElement( 'h3' );
-            const taglineText  = document.createElement( 'p' );
-            const priceText  = document.createElement( 'p' );
-            /*h2.textContent = name;*/
-            h3.textContent = data._city;
-            taglineText.textContent = data._tags;
-            priceText.textContent = data._price + "€";
-            article.classList.add("artist-post");
-            article.appendChild(img);
-            article.appendChild(videotag);
-            videotag.appendChild(videosource);
-            article.appendChild(h2);
-            article.appendChild(h3);
-            article.appendChild(taglineText);
-            article.appendChild(priceText);
-            let imgAricle = data.image;
-            //article.sort((a,b)=>a-b)
 
-            /*h2.insertAdjacentHTML('afterbegin',
-            '<a href="`?id=${photographer.id}`;">');*/        
+        const h2 = document.createElement( 'h2' );
+        const a = document.createElement( 'a' );
+        a.setAttribute("href", "photographer.html?id=" + data._id); 
+        a.innerHTML = data._title;     
+        h2.appendChild(a);
+        const h3 = document.createElement( 'h3' );
+        const taglineText  = document.createElement( 'p' );
+        const priceText  = document.createElement( 'p' );
+        h3.textContent = data._city;
+        taglineText.textContent = data._tags;
+        priceText.textContent = data._price + "€";
+        article.classList.add("artist-post");
+        article.appendChild(h2);
+        article.appendChild(h3);
+        article.appendChild(taglineText);
+        article.appendChild(priceText);
+
+      
+
+                
             return (article);
         }
 
@@ -154,7 +162,7 @@ class CardDOM
           div.appendChild(taglineTag);
           photographerHeader.appendChild(button);
           photographerHeader.appendChild(imgTag);
-                  }
+    }
 
 }
 
--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -7,57 +7,49 @@ const artistId = pageId.get('id');
 
 const photographerHeader = document.querySelector(".photograph-header");
 
-
 const mediasData = jsonData.media.filter((el)=>{
     return el.photographerId == artistId;
 })
-
 
 const currentPhotographers = jsonData.photographers.filter((el)=>{
     return el.id == artistId;
 });
 
 
-
+let medias = [];
 //recupere la section du DOM contenant les photographes
-async function displayData() {
-
-
-
+async function displayDataCurrentUser()
+{
     const currentPhotographer = currentPhotographers[0];
 
     const currenttemplateCard = new CardDOM(currentPhotographer); 
 
     currenttemplateCard.getCurrentUserCardDOM(currentPhotographer);
-
-
-
-//const currenttemplateCard = new CardDOM(currentPhotographer); 
-//const currentuserCardDOM = currenttemplateCard.getCurrentUserCardDOM(currentPhotographer);
-
-//photographerHeader.appendChild(currentuserCardDOM);
-
-
+}
+async function displayData(data) {
     //recupere la div contenant les cartes de photographe
     const photographersSection = document.querySelector(".all-posts");
 
-
    
+    if(data)
+    {
+        medias = data;
+    }
+    else{
+        mediasData.filter((el)=>{
+             if(el.hasOwnProperty('image'))
+             {
+                return medias = mediasData.map(element =>  new FactoryBuilder(element, "photoArtwork"));
+             }
+             else if(el.hasOwnProperty('video'))
+             {
+                return medias = mediasData.map(element =>  new FactoryBuilder(element, "videoArtwork"));
+             }
+           });
+          
+    }
 
-    videoData = mediasData.filter((el)=>{
-        return el.hasOwnProperty('video')
-       });
-    photoData = mediasData.filter((el)=>{
-        return el.hasOwnProperty('image')
-       });
-    
-    const videos = videoData.map(element =>  new FactoryBuilder(element, "videoArtwork"));
-    const photos = photoData.map(element =>  new FactoryBuilder(element, "photoArtwork"));
-    
-    medias = videos.concat(photos);
     let desc = false;
-
-
     function sortArrayBy(array, sort,desc)
     {
         array.sort(function (a,b) {
@@ -71,90 +63,60 @@ async function displayData() {
         if(desc) array.reverse();
         return array;
     }
-
-
-    
- 
-    
-  
     medias.forEach((media) => {
         //console.log(media)
         const templateCard = new CardDOM(media);
  
         const userCardDOM = templateCard.getMediaCardDOM(media);
 
-        photographersSection.appendChild(userCardDOM);
-        
-
- 
-    });          
-    
+        photographersSection.appendChild(userCardDOM); 
+    });            
 };
 
 async function init() {
     // Récupère les datas des photographes
     displayData();
-    //tri des medias
+    displayDataCurrentUser();
     //recuperer les articles
     let articles = document.getElementsByClassName('artist-post');
-    //recuperer le click du selcect 
+    let desc = false;
 
 
+    function sortArrayBy(array, sort,desc)
+    {
+        array.sort(function (a,b) {
+            if(a[sort] < b[sort]) return -1;
 
 
-
-
-
-
-    //filters
-    document.querySelectorAll(".home_filters a").forEach(tagDOM=>{
-        tagDOM.addEventListener("click", (e)=>{
-            let container = document.getElementsByClassName("photographer_section")[0];
-              container.innerHTML = "";
-
-             const currentDOMtag =  e.target.innerHTML;
-
-              const tag = e.target.dataset.filter;
-
-              const vals = photographers.filter(el => el.tags.find(l => l == tag));
-
-              displayData(vals);
+            if(a[sort] > b[sort]) return 1;
+            return 0;
+            
         });
+        if(desc) array.reverse();
+        return array;
+    }
+
+    //nom de la proprieter utilisée pour le tri
+    let sortProperty = "";
+
+    let sortingSelect = document.querySelector('#sorting-select');
+    sortingSelect.addEventListener("change",(e)=>{
+            
+        const allPosts = document.getElementsByClassName('all-posts');
+        allPosts[0].innerHTML = "";   
+        if(e.target.value == "popularite")
+        {
+            sortProperty = "_likes";
+        }
+        else if(e.target.value == "titre")
+        {
+            sortProperty = "_title";
+        }
+        else if(e.target.value == "date"){
+                sortProperty = "_date";
+        }
+        displayData(sortArrayBy(medias,sortProperty,desc));
     });
-    //filters of projects
- 
-
-let desc = false;
-
-
-function sortArrayBy(array, sort,desc)
-{
-    array.sort(function (a,b) {
-        if(a[sort] < b[sort]) return -1;
-
-
-        if(a[sort] > b[sort]) return 1;
-        return 0;
-        
-    });
-    if(desc) array.reverse();
-    return array;
-}
-const sortedMedias = "";
-
-let sortingSelect = document.querySelector('#test');
-sortingSelect.addEventListener("click",(e)=>{
-    const allPosts = document.getElementsByClassName('all-posts');
-    allPosts[0].innerHTML = "";
-    console.log('supp')
-    
-//conditions
-const medias = sortArrayBy(medias,'_title',desc);
-console.log("clicked")
-console.log(sortedMedias);
-medias.innerHTML = "";
-displayData(medias);
-});
 }
 
 init();
